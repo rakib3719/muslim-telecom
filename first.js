6d7c7a94ec4775f9f41339b250fdcdd7cbbef3ca
rakib3719 <bannah76769@gmail.com>
2024-02-26 15:21:16 +0600

    robi done

---
+++ first.js
@@ -0,0 +1,170 @@
+// let str = [`
+// 🟦 80+1800  950 Tk all
+// 🟦  50+1500  799 Tk all
+// 🟦80 gb 680 Tk all
+// 🟦60 gb 599 Tk all
+// 🟦40 gb 540 Tk all
+// 🟦670m 399 Tk all
+// 🟦575m 350 Tk all
+// 🟦380m 250 Tk all
+// 🟦Robi 50GB 560 Tk a
+// 🟦Robi 25GB 310 Tk al
+// _*(২৫/২০জিবি ৭+৭+৭+৭
+// 💚Robi 20+200===39্‌
+// 💚Robi 20+300===420
+// 💚Robi 20+400===470
+// 💚Robi 20+500===54০
+// 💚Robi 20+700===650
+// 💚Robi 20+1000==770
+// 💚Robi 25+200===420
+// 💚Robi 25+300===420
+// 💚Robi 25+400===480
+// 💚Robi 25+500===560
+// 💚Robi 25+700===670
+// 💚Robi 25+1000==790
+// 🔵Robi 30+100----590৳
+// 🔵Robi 30+200----620৳
+// 🔵Robi 30+300----620৳
+// 🔵Robi 30+400----655৳
+// 🔵Robi 30+500----655৳
+// 🔵Robi 30+700----699৳
+// 🔵Robi 30+1000---799৳
+// 🔵Robi 40+100----630৳
+// 🔵Robi 40+200----650৳
+// 🔵Robi 40+300----660৳
+// 🔵Robi 40+400----640
+// 🔵Robi 40+500----690
+// 🔵Robi 40+700----725৳
+// 🔵Rovi 40+1000---799৳
+// 🔵Robi 50+100----690৳
+// 🔵Robi 50+200----690৳
+// 🔵Robi 50+300----699৳
+// 🔵Robi 50+400----740
+// 🔵Robi 50+500----740৳
+// 🔵Robi 50+700----770
+// 🔵Robi 50+1000---805
+// 💚Robi 75+100===740
+// 💚Robi 75+200===750
+// 💚Robi 75+300===755
+// 💚Robi 75+400===765
+// 💚Robi 75+500===780
+// 💚Robi 75+700----805
+// 💚Robi 75+1000===840
+// 💚Robi 100+100===765
+// 💚Robi 100+200===799
+// 💚Robi 100+300===810
+// 💚Robi 100+500===825
+// 💚Robi 100+700===850
+// 💚Robi 100+1000===880
+
+// `]
+
+let ronbiOffer = `80 GB +1800 Minite  950 Tk all
+50 Gb  + 1500 Minite  799 Tk all
+80  gb 680 Tk all
+60  gb 599 Tk all
+40  gb 540 Tk all
+670m 399 Tk all
+575m 350 Tk all
+380m 250 Tk all
+Robi 50GB 560 Tk all
+Robi 25GB 310 Tk all
+_*(২৫/২০জিবি ৭+৭+৭+৭)
+Robi 20 GB + 200 Minite === 399 Tk
+Robi 20 GB + 300 Minite === 420 Tk
+Robi 20 GB + 400 Minite === 470 Tk
+Robi 20 GB + 500 Minite === 540 Tk
+Robi 20 GB + 700 Minite === 650 Tk
+Robi 20 GB + 1000 Minite ==770 Tk
+Robi 25 GB + 200 Minite ===420 Tk
+Robi 25 GB + 300 Minite ===420 Tk
+Robi 25 GB + 400 Minite ===480 Tk
+Robi 25 GB + 500 Minite ===560 Tk
+Robi 25 GB + 700 Minite ===670 Tk
+Robi 25 GB + 1000 Minitte ==790 Tk
+Robi 30 GB + 100 Minite ----590 Tk
+Robi 30 GB + 200 Minite ----620৳ Tk
+Robi 30 GB + 300 Minite ----620৳ Tk
+Robi 30 GB + 400 Minite ----655৳ Tk
+Robi 30 GB + 500 Minite ----655৳ Tk
+Robi 30 GB + 700 Minite ----699৳ Tk
+Robi 30 GB + 1000 Minite ---799৳ Tk
+Robi 40 GB + 100 Minite ----630৳ Tk
+Robi 40 GB + 200 Minite ----650৳ Tk
+Robi 40 GB + 300 Minite ----660৳ Tk
+Robi 40 GB + 400 Minite ----640 Tk
+Robi 40 GB + 500 Minite ----690 Tk
+Robi 40 GB + 700 Minite ----725৳ Tk
+Rovi 40 GB + 100 Minite 0---799৳ Tk
+Robi 50 GB + 100 Minite ----690৳ Tk
+Robi 50 GB + 200 Minite ----690৳ Tk
+Robi 50 GB + 300 Minite ----699৳ Tk
+Robi 50 GB + 400 Minite ----740 Tk
+Robi 50 GB + 500 Minite ----740৳ Tk
+Robi 50 GB + 700 Minite ----770৳ Tk
+Robi 50 GB + 1000 Minite ---805 TK
+Robi 75 GB + 100 Minite ===740 TK
+Robi 75 GB + 200 Minite ===750 TK
+Robi 75 GB + 300 Minite ===755 TK
+Robi 75 GB + 400 Minite ===765 TK
+Robi 75 GB + 500 Minite ===780 TK
+Robi 75 GB + 700 Minite ----805 TK
+Robi 75 GB + 1000 Minite ===840 TK
+Robi 100 GB + 100 Minite ===765 TK
+Robi 100 GB + 300 Minite ===810 TK
+Robi 100 GB + 400 Minite ===810 TK
+Robi 100 GB + 500 Minite ===825 TK
+Robi 100 GB + 700 Minite ===850 TK
+Robi 100 GB + 1000 Minite ===880 TK
+
+`;
+
+let con = ronbiOffer.split(`
+`);
+for(const robiList of con){
+
+
+
+
+let p1 =  document.createElement('p');
+    p1.innerText = robiList;
+    
+    let appendRobi = document.getElementById('appendRobi');
+    appendRobi.appendChild(p1)
+    p1.classList.add('list-item')
+
+
+}
+
+document.getElementById('robi').addEventListener('click', function(){
+
+
+
+    hideMain ()
+    ShowDiv()
+
+    const appnedRobiShow = document.getElementById('appendRobi');
+    appnedRobiShow.classList.remove('hidden')
+
+
+})
+
+function hideMain (){
+
+  const main = document.getElementById('main');
+  main.classList.add('hidden')
+
+}
+
+function ShowDiv(){
+
+    const listDiv = document.getElementById('list-div');
+    listDiv.classList.remove('hidden')
+}
+
+document.getElementById('close').addEventListener('click', function (){
+
+
+location.reload()
+
+})
